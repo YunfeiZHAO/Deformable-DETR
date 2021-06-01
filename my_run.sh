6f3de29588cc4fe355b22ea85437bc55b606494f
YunfeiZHAO <moizhaoyunfei@gmail.com>
2021-06-01 11:32:55 +0200

    my_run.sh update

--- my_run.sh
+++ my_run.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-python main.py
+# python main.py --output_dir ./run --coco_path ~/Desktop/coco --lr 0.0002 --lr_backbone 0.00001 --num_queries 300 --batch_size 1 --enc_layers 6 --dec_layers 6 --no_aux_loss
+
+python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1  --node_rank 0  --use_env main.py \
 --output_dir ./run \
---coco_path ~/Desktop/coco \
+--coco_path ~/coco \
 --lr 0.0002 \
 --lr_backbone 0.00001 \
 --num_queries 300 \
 --batch_size 1 \
+--epochs 300 \
+--num_workers 2 \
 --enc_layers 6 \
 --dec_layers 6 \
---no_aux_loss
-
-python main.py --output_dir ./run --coco_path ~/Desktop/coco --lr 0.0002 --lr_backbone 0.00001 --num_queries 300 --batch_size 1 --enc_layers 6 --dec_layers 6 --no_aux_loss
+--no_aux_loss \
+--scales 1 \
